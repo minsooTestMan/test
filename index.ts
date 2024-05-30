9d84778ad3f3035074fab714170e8ad777fa4008
minsoo <minsoo@giraffeailabs.com>
2024-05-30 14:34:13 +0900

    feat : test

---
+++ index.ts
@@ -0,0 +1,37 @@
+import * as http from "http";
+
+const express = require("express");
+const app = express();
+// const http = require("http")
+const io = require('socket.io');
+
+const httpServer = http.createServer(app);
+
+const socketServer = io(httpServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// type test = {
+//     data:string;
+// }
+
+const testArr = []
+
+socketServer.on("connection", (socket:any) => {
+    console.log(`User Connected: ${socket.id}`);
+    socket.on('join', (data:any, callback:(err:string)=>void) => {
+        console.log(data)
+    })
+}); // 연결확인
+
+socketServer.on("disconnect", (socket:any) => {
+    console.log(`User Disconnect: ${socket.id}`);
+    socket.on('join', (data:any, callback:(err:string)=>void) => {
+        console.log(data)
+    })
+}); // 연결확인
+
+httpServer.listen(3001)
