c4ab8232a1d2116f6eae4fa8391176ef16bc0c03
minsoo <minsoo@giraffeailabs.com>
2024-05-30 17:49:37 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,10 @@ const app = express();
 const io = require('socket.io');
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3002',
+    credentials:true
+}))
 // app.use(cors({
 //     origin:'http://localhost:3002',
 //     Credentials:true
