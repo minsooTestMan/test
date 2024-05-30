0bb1000452fe2e6cc00c0ad66948b99c610b7e13
minsoo <minsoo@giraffeailabs.com>
2024-05-30 18:20:40 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,10 @@ app.get('/route', function(req:any, res:any) {
     res.send('Test')
 });
 app.post('/cookie', function(req:any, res:any) {
+    res.writeHead(200,{
+        "Access-Control-Allow-Credentials": "true",
+        origin: 'http://localhost:3002'
+    })
     res.cookie('refreshToken', 'testan',{
         httpOnly:true,
         expires: new Date(Date.now() + 3600000)
